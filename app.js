e9f8b92929e72106affd48fca38e1595f4558356
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-23 18:26:02 +0700

    js lab 138

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-// write code here
+// จงหาค่ามากสุดและน้อยสุดใน arr
+
+let arr = [3.24, 2.78, 3.86, 1.37, 0.52];
+
+const min = Math.min(...arr);
+const max = Math.max(...arr);
+console.log(`Min number: ${min}
+Max number: ${max}`);
